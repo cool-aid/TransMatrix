40a5bd2f5598fa58902c9f331b87c56c2078103f
cool-aid <blu186096@gmail.com>
2024-11-17 21:03:34 -0800

    refactor: constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,14 @@
-const SUPPORTED_LANGUAGES = {
-  'auto': 'Auto Detect',
-  'en': 'English',
-  'es': 'Spanish',
-  'fr': 'French',
-  'de': 'German',
-  'it': 'Italian',
-  'ja': 'Japanese',
-  'ko': 'Korean',
-  'zh': 'Chinese'
-};
-
 const MAX_TARGET_LANGUAGES = 3;
+
+const SUPPORTED_LANGUAGES = [
+  { code: "en", name: "English" },
+  { code: "es", name: "Spanish" },
+  { code: "fr", name: "French" },
+  { code: "de", name: "German" },
+  { code: "it", name: "Italian" },
+  { code: "ja", name: "Japanese" },
+  { code: "ko", name: "Korean" },
+  { code: "zh", name: "Chinese" },
+];
+
+export { MAX_TARGET_LANGUAGES, SUPPORTED_LANGUAGES };
